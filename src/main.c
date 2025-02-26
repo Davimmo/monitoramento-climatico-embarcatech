96c9e769f8d9d033358ab26125e7efc276f6df31
Davi BC <davi20cavalcanti@gmail.com>
2025-02-26 15:22:24 -0300

    Ajustes no controle dos leds com o joystick

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@
 
 #define LOG(var) printf("%s: %d\n", #var, var);
 #define TEMPO_DEBOUNCE 150
-#define LIMIAR 50
+#define LIMIAR 150
 
 
 
@@ -29,11 +29,10 @@
 void verificar_joystick(int16_t x, int16_t y) {
     //Intensidade dos leds vária de acordo com o módulo do vetor direção
 
-    uint8_t maior_coordenada;
+    uint8_t intensidade;
     int16_t x_absoluto=abs(x);
     int16_t y_absoluto=abs(y);
-    maior_coordenada = (x_absoluto> y_absoluto) ? x : y;
-    maior_coordenada = abs(maior_coordenada);
+    intensidade = x_absoluto>y_absoluto?x:y;
     
     bool sol = false, chuva = false, vento = false;
     int direcao = -1;
@@ -76,68 +75,72 @@ void verificar_joystick(int16_t x, int16_t y) {
             led_intensity(LED_GREEN,0);
             led_intensity(LED_BLUE,0);
             led_intensity(LED_RED,0);
+            printf("Caso -1\n");
             break;
         case 0:
             led_intensity(LED_GREEN,0);
             led_intensity(LED_BLUE,0);
             led_intensity(LED_RED,0);
+            printf("Caso 0: Norte\n");
             break; // Norte
         case 1:
             vento = true;
-            led_intensity(LED_GREEN,maior_coordenada);
+            led_intensity(LED_GREEN,intensidade);
 
             led_intensity(LED_RED,0);
             led_intensity(LED_BLUE,0);
+            printf("Caso 1: Nordeste\n");
             break; // Nordeste
         case 2:
             chuva = true;
-            led_intensity(LED_BLUE,maior_coordenada);
+            led_intensity(LED_BLUE,intensidade);
+            LOG(intensidade);
 
             led_intensity(LED_BLUE,0);
             led_intensity(LED_GREEN,0);
+            printf("Caso 2: Leste\n");
             break; // Leste
         case 3:
             chuva = true; 
             vento = true;
-            led_intensity(LED_GREEN,maior_coordenada); 
-            led_intensity(LED_BLUE,maior_coordenada); 
+            led_intensity(LED_GREEN,intensidade); 
+            led_intensity(LED_BLUE,intensidade); 
             led_intensity(LED_RED,0); 
+            printf("Caso 3: Sudeste\n");
             break; // Sudeste
         case 4:
             sol = true;
-            led_intensity(LED_RED,maior_coordenada); 
+            led_intensity(LED_RED,intensidade); 
             led_intensity(LED_GREEN,0); 
-            led_intensity(LED_BLUE,0); 
+            led_intensity(LED_BLUE,0);
+            printf("Caso 4: Sul\n"); 
             break; // Sul
         case 5:
-            sol = true; vento = true; 
-            led_intensity(LED_RED,maior_coordenada);
-            led_intensity(LED_GREEN,maior_coordenada);
+            sol = true;
+            vento = true; 
+            led_intensity(LED_RED,intensidade);
+            led_intensity(LED_GREEN,intensidade);
             led_intensity(LED_BLUE,0);
+            printf("Caso 5: Sudeste\n");
             break; // Sudoeste
         case 6:
             sol = true;
             chuva = true; 
-            led_intensity(LED_RED,maior_coordenada);
-            led_intensity(LED_BLUE,maior_coordenada);
+            led_intensity(LED_RED,intensidade);
+            led_intensity(LED_BLUE,intensidade);
             led_intensity(LED_GREEN,0);
+            printf("Caso 6: Oeste\n");
             break; // Oeste
         case 7:
             sol = true; 
             chuva = true; 
             vento = true; 
-            led_intensity(LED_GREEN,maior_coordenada);
-            led_intensity(LED_BLUE,maior_coordenada);
-            led_intensity(LED_RED,maior_coordenada);
+            led_intensity(LED_GREEN,intensidade);
+            led_intensity(LED_BLUE,intensidade);
+            led_intensity(LED_RED,intensidade);
+            printf("Caso 7: Noroeste \n");
             break; // Noroeste
     }
-    
-    bool recolhidas = (x > LIMIAR || x < -LIMIAR || y > LIMIAR || y < -LIMIAR);
-    printf("Roupas %s, Sol: %s, Chuva: %s, Vento: %s\n", 
-           recolhidas ? "recolhidas" : "estendidas", 
-           sol ? "true" : "false", 
-           chuva ? "true" : "false", 
-           vento ? "true" : "false");
 }
 
 
@@ -160,6 +163,5 @@ int main()
         int16_t joystick_y = joystick_read(JOYSTICK_Y_PIN, 10, 510);
 
         verificar_joystick(joystick_x, joystick_y);
-        sleep_ms(10);
     }
 }
